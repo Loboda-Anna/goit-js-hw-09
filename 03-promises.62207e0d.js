b1588dff22323dc5e601077bea9c3e5cc66b0735
Loboda-Anna <107415190+Loboda-Anna@users.noreply.github.com>
2023-01-13 15:33:53 +0000

    Deploying to gh-pages from @ Loboda-Anna/goit-js-hw-09@194304c7a7eb744b1f52910e866b1ee13fda7b16 🚀

---
+++ 03-promises.62207e0d.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".form"),t=(document.querySelector('.form input[name="delay"'),document.querySelector('.form input[name="step"'),document.querySelector('.form input[name="number"'),document.querySelector('.form button[type="submit"'));e.addEventListener("submit",(function(e){e.preventDefault(),Math.random()})),console.log(t);
+//# sourceMappingURL=03-promises.62207e0d.js.map
