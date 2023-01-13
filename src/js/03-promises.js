194304c7a7eb744b1f52910e866b1ee13fda7b16
Loboda-Anna <107415190+Loboda-Anna@users.noreply.github.com>
2023-01-13 17:33:24 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,16 @@
+const formEl = document.querySelector('.form');
+const delayEl = document.querySelector('.form input[name="delay"');
+const stepEl = document.querySelector('.form input[name="step"');
+const numberEl = document.querySelector('.form input[name="number"');
+const submitBtn = document.querySelector('.form button[type="submit"');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  createPromise();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +19,4 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+console.log(submitBtn);
