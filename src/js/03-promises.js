faa61f9fe460b34f6d606f07a48754e9c2e0db2f
Loboda-Anna <107415190+Loboda-Anna@users.noreply.github.com>
2023-01-17 16:33:35 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,4 @@
 import Notiflix from 'notiflix';
-import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.form');
 const delayEl = document.querySelector('.form input[name="delay"]');
@@ -8,48 +7,32 @@ const amountEl = document.querySelector('.form input[name="amount"]');
 const submitBtn = document.querySelector('.form button[type="submit"]');
 
 formEl.addEventListener('submit', onFormSubmit);
-formEl.addEventListener('input', throttle(onFormInput, 500));
-let formData = {};
-
-function onFormInput(event) {
-  formData = {
-    firstDelay: delayEl.value,
-    delay: stepEl.value,
-    amount: amountEl.value,
-  };
-}
 
 function onFormSubmit(event) {
   event.preventDefault();
-  setTimeout(() => {
-    for (let position = 1; position <= formData.amount; position += 1) {
-      let delay = formData.delay * position;
-      createPromise(position, delay)
-        .then(({ position, delay }) => {
-          Notiflix.Notify.success(
-            `✅ Fulfilled promise ${position} in ${delay}ms`
-          );
-        })
-        .catch(({ position, delay }) => {
-          Notiflix.Notify.failure(
-            `❌ Rejected promise ${position} in ${delay}ms`
-          );
-        });
-    }
-  }, formData.firstDelay);
+  const { delay, step, amount } = event.target.elements;
+  let delayValue = +delay.value;
+  for (let position = 1; position <= amount.value; position += 1) {
+    createPromise(position, delayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayValue += +step.value;
+  }
 }
-
 function createPromise(position, delay) {
   const promise = new Promise((res, rej) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
         res({ position, delay });
-        let resolveObj = {
-          position,
-          delay,
-        };
-        console.log(resolveObj);
       } else {
         rej({ position, delay });
       }
